4ca3cc46906f2875f3232b55c37d52327af60d9a
Calliope7 <7fergies@gmail.com>
2015-06-18 08:13:47 +0100

    Created first cut of Persistent matrix and inverse functions to work with it.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        thisInverse <- NULL
+        set <- function(someMatrix) {
+                x <<- someMatrix
+                thisInverse <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) thisInverse <<- inverse
+        getInverse <- function() thisInverse
+        list(set = set, 
+             get = get, 
+             setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getInverse()
+        if (!is.null(inverse)) {
+                message("getting cached inverse")
+                return (inverse)
+        }
+        matrix <- x$get()
+        inverse <- solve(matrix)
+        x$setInverse(inverse)
+        inverse
 }
